1e52b00fdbeda52633f4d4f3e5405f24a08a2e25
Subrina Momtaz <140344787+Subrina00@users.noreply.github.com>
2025-12-10 11:32:50 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router } from 'react-router-dom'
+import './App.css'
+import Sidebar from './components/Sidebar'
+
+function App() {
+  
+
+  return (
+    <Router>
+      <div className="flex h-screen">
+        <Sidebar />
+      </div>
+    </Router>
+  )
+}
+
+export default App
