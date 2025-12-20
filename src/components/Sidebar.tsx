2b408f7c77bba539d6ebba5bb4da8fbad8736137
Subrina Momtaz <140344787+Subrina00@users.noreply.github.com>
2025-12-21 01:39:20 +0600

    context

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { useFilter } from './FilterContext';
 
 interface Product {
   category: string;
@@ -11,6 +12,14 @@ interface FetchResponse {
 
 
 const Sidebar = () => {
+
+  const { 
+        searchQuery, setSearchQuery,
+        selectedCategory, setSelectedCategory,
+        minPrice, setMinPrice,
+        maxPrice, setMaxPrice,
+        keyword, setKeyword,
+   } = useFilter();
 
   const [categories, setCategories] = useState<string[]>([]);
   const [keywords] = useState<string[]>(["apple", "watch", "fashion", "trend", "shoes", "shirt"]);
@@ -35,16 +44,48 @@ const Sidebar = () => {
     fetchCategories();
   }, [])
 
+
+
+  const handleMinPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setMinPrice(value ? parseFloat(value) : undefined);
+  }
+
+  const handleMaxPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setMaxPrice(value ? parseFloat(value) : undefined);
+  }
+
+  const handleRadioChangeCategories = (category: string) => {
+    setSelectedCategory(category);
+  }
+
+  const handleKeywordClick = (keyword: string) => {
+    setKeyword(keyword);
+  }
+
+  const handleResetFilters = () => {
+    setSearchQuery("");
+    setSelectedCategory("");
+    setMinPrice(undefined);
+    setMaxPrice(undefined);
+    setKeyword("");
+  }
+
   return (
     <div className='w-64 p-5 h-screen'>
       <h1 className='text-2xl font-bold mb-10 mt-4'>React Store</h1>
 
       <section>
-        <input type="text" className='border-2 rounded px-2 sm:mb-0' placeholder='Search product' />
+        <input type="text" className='border-2 rounded px-2 sm:mb-0'
+         placeholder='Search product' value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)} />
 
         <div className="flex justify-center items-center">
-          <input type="text" className='border-2 mr-2 px-5 py-3 mb-3 w-full' placeholder='Min' />
-          <input type="text" className='border-2 mr-2 px-5 py-3 mb-3 w-full' placeholder='Max' />
+          <input type="text" className='border-2 mr-2 px-5 py-3 mb-3 w-full' placeholder='Min'
+          value={minPrice ?? ""} onChange={handleMinPriceChange} />
+          <input type="text" className='border-2 mr-2 px-5 py-3 mb-3 w-full' placeholder='Max'
+          value={maxPrice ?? ""} onChange={handleMaxPriceChange} />
         </div>
 
         {/* categories */}
@@ -56,7 +97,9 @@ const Sidebar = () => {
         <section>
           {categories.map((category, index) => (
             <label key={index} className='block mb-2'>
-              <input type="radio" name='category' value={category} className='mr-2 w-[16px] h-[16px]' />
+              <input type="radio" name='category' value={category}
+              onChange={() => handleRadioChangeCategories(category)}
+              checked={selectedCategory === category} className='mr-2 w-[16px] h-[16px]' />
               {category.toUpperCase()}
             </label>
           ))}
@@ -68,12 +111,14 @@ const Sidebar = () => {
           <h2 className='text-xl font-semibold mb-3'>Keywords</h2>
           <div>
             {keywords.map((keyword, index) => (
-              <button key={index} className='block mb-2 px-4 w-full text-left border rounded hover:bg-gray-200'>{keyword.toUpperCase()}</button>
+              <button key={index}
+              onClick={() => handleKeywordClick(keyword)} className='block mb-2 px-4 w-full text-left border rounded hover:bg-gray-200'>{keyword.toUpperCase()}</button>
             ))}
           </div>
         </div>
 
-        <button className='w-full mb-[4rem] py-2 bg-black text-white rounded mt-5'>Reset Filters</button>
+        <button onClick={handleResetFilters}
+         className='w-full mb-[4rem] py-2 bg-black text-white rounded mt-5'>Reset Filters</button>
 
         
       </section>
